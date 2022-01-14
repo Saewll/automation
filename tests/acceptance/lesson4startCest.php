bbf42319ab08fa068791343d56d58746e42c624a
Saewl <valdislavv.kon@gmail.com>
2022-01-14 17:50:47 +0600

    new

--- tests/acceptance/lesson4startCest.php
+++ tests/acceptance/lesson4startCest.php
@@ -5,10 +5,25 @@ class lesson4startCest
      // Тест на поиск блузки в модальном окне на сайте
      public function checkSearch(AcceptanceTester $I)
      {
-     
+      $moveMouseOberCss='img[alt="Blouse" ]';
+      $clickCss='#homefeatured > li:nth-child(2)';
+      $waitForElementVisibleCss='#index > div.fancybox-overlay.fancybox-overlay-fixed';
+      $switchToIframeCss='.fancybox-iframe';
+      $moveMouseOberXPath='//img[@title="Blouse"]';
+      $clickXPath='//*[@id="homefeatured"]/li[2]';
+      $waitForElementVisibleXPath='//*[@id="index"]/div[2]';
+      $switchToIframeXPath='//*[@class="fancybox-iframe"]';
+      
+
+
+
+
+
+
+
       $I->amOnPage('');
       $I->see('Blouse');
-      $I->moveMouseOver('#homefeatured > li:nth-child(2) > div > div.left-block > div > a.product_img_link > img');
+      $I->moveMouseOver('img[alt="Blouse" ]');
       $I->click('#homefeatured > li:nth-child(2)');
       $I->waitForElementVisible('#index > div.fancybox-overlay.fancybox-overlay-fixed');
       $I->switchToIFrame('.fancybox-iframe');
