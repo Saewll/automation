c065985eff4871ab477600dd09145acf34e98265
Vladislav Kon <vladislavv.kon@gmail.com>
2022-01-18 14:10:19 +0600

    new

--- tests/functional/lesson4startCest.php
+++ tests/functional/lesson4startCest.php
@@ -4,10 +4,23 @@ use Helper\Functional;
 
 class ArticlePageCest
 {
-
+    
     // tests
     public function checkArticlePageList(FunctionalTester $I)
     {
+    $clickSearchFIeldCss='#search_query_top';
+    $clickSEarchButtonCss='#searchbox > button';
+    $seeNumberOfElementsCss='.product-container';
+    
+    $seeElementXPath='//input[@id="search_query_top"]';
+    $clickSEarchButtonXPath='//form[@id="searchbox"]//button';
+    $seeNumberOfElementsCss='//div[@class="product-container"]';
+
+
+
+
+
+
         $I->amOnPage('');
         $I->seeElement('#search_query_top');      
         $I->click('#search_query_top');
